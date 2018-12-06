76f2f4786fccc63053e4a166431c86621ffcdcc9
jasonpham26 <vinhphuong0206@gmail.com>
2018-12-06 19:49:00 +1000

    Updates

---
+++ precache-manifest.0616ccc892fc29460f00b7598d340bff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1a95c78c8b1e07287e07",
+    "url": "/roboFriends/static/js/runtime~main.1a95c78c.js"
+  },
+  {
+    "revision": "a29e11bd988573ab4cd7",
+    "url": "/roboFriends/static/js/main.a29e11bd.chunk.js"
+  },
+  {
+    "revision": "3831ea6675e52ffdaf56",
+    "url": "/roboFriends/static/js/1.3831ea66.chunk.js"
+  },
+  {
+    "revision": "a29e11bd988573ab4cd7",
+    "url": "/roboFriends/static/css/main.f9472d3e.chunk.css"
+  },
+  {
+    "revision": "3831ea6675e52ffdaf56",
+    "url": "/roboFriends/static/css/1.5bd94381.chunk.css"
+  },
+  {
+    "revision": "22600342426a42a722d1a2161a889f9c",
+    "url": "/roboFriends/index.html"
+  }
+];
